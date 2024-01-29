77bb326b4fefd22eef3f703036a636839c475e2c
Cayla Bradley <cayla.bradley94@gmail.com>
2024-01-29 10:07:49 -0600

    Hello World in Javascript

---
+++ JavaScript/foundations/hello_world.js
@@ -0,0 +1,8 @@
+// "Hello, World!"
+console.log("Hello, World!"); //instructs the console to print the string "Hello, World!" to the console
+
+// `console`: Refers to the JavaScript console, a built-in object in web browsers and some JS environments that provides a way for developers to output messages, warnings, and errors for debugging and development purposes
+
+// `.log()`: a method of the `console` object that can take one or more arguments and print them to the console
+
+// `("Hello, World!)`: The argument, in this case a String, passed to the `log()` method and printed to the console
