7f597cfdebf0ad537cbac713d36d88106573c5be
Cayla Bradley <cayla.bradley94@gmail.com>
2024-01-29 12:05:07 -0600

    create .csproj

--- CSharp/foundations/HelloWorld.cs
+++ CSharp/foundations/HelloWorld.cs
@@ -1,19 +1,11 @@
 // "Hello World"
 
-using System
+using System; //namespace which contains fundamental types and the `Console` class for input and output operations.
 
-class Program
+class Program // Defines a class named `Program`.
 {
-  static void Main()
+  static void Main() //Declares the entry point of the program, where the `Main` method is called when the program starts.
   {
-    Console.WriteLine("Hello, World");
+    Console.WriteLine("Hello, World"); //outputs the string "Hello, World!" followed by a newline to the console.
   }
-}
-
-// `using System;`: namespace which contains fundamental types and the `Console` class for input and output operations.
-
-// `class Program`: Defines a class named `Program`.
-
-// `static void Main()`: Declares the entry point of the program, where the `Main` method is called when the program starts.
-
-// `Console.WriteLine("Hello, World!");`: outputs the string "Hello, World!" followed by a newline to the console.
+};
