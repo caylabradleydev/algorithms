43cb403a66ff914a501ed876156069f660da153d
Cayla Bradley <cayla.bradley94@gmail.com>
2024-01-29 10:32:06 -0600

    HelloWorld.cs

---
+++ CSharp/foundations/HelloWorld.cs
@@ -0,0 +1,19 @@
+// "Hello World"
+
+using System
+
+class Program
+{
+  static void Main()
+  {
+    Console.WriteLine("Hello, World");
+  }
+}
+
+// `using System;`: namespace which contains fundamental types and the `Console` class for input and output operations.
+
+// `class Program`: Defines a class named `Program`.
+
+// `static void Main()`: Declares the entry point of the program, where the `Main` method is called when the program starts.
+
+// `Console.WriteLine("Hello, World!");`: outputs the string "Hello, World!" followed by a newline to the console.
