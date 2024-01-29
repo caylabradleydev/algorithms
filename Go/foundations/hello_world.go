90c3e2ff85e9c4044d1f4608a7594eef3f86fa4a
Cayla Bradley <cayla.bradley94@gmail.com>
2024-01-29 10:45:03 -0600

    hello_world.go

---
+++ Go/foundations/hello_world.go
@@ -0,0 +1,9 @@
+// "Hello, World!"
+
+package main //Specifies that this Go file is part of the `main` package, which is the starting point for the execution of the program
+
+import "fmt" //Imports the `fmt` package, which provide functions for formatted I/O (including Println())
+
+func main() { //Declares the `main` function, which is the entry point of the program
+	fmt.Println("Hello, World!") //Outputs the string "Hello, World!" followed by a newline to the console.
+}
