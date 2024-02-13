da27ff1e1515687e5ad2cf042fda09fcf24d9285
Cayla Bradley <caylabradleydev@gmail.com>
2024-02-13 09:48:23 -0600

    Conditional Statements

--- JavaScript/foundations/04_stringsAndTemplateLiterals.js
+++ JavaScript/foundations/04_stringsAndTemplateLiterals.js
@@ -2,6 +2,10 @@
 //====VARIABLES====//
 //=================//
 
+//Concatenation: a technique used in programming to merge two or more strings into one value
+
+//concatenation operator (+): concatenates two or more string values together and return another string which is the union of the two operand
+
 //Template literals: literals delimited with backtick (`) characters, allowing for multi-line strings, string interpolation with embedded expressions, and special constructs called tagged templates.
 
 const firstName = "Cayla";
@@ -9,7 +13,7 @@ const job = "software developer";
 const birthYear = 1991;
 const currentYear = 2024;
 
-//Using Concatenation
+//Using Concatenation Operator
 const caylaGreeting =
   "I'm " +
   firstName +
