6dbff2de9288aad6c89a412ae182f3f399a77909
Cayla Bradley <caylabradleydev@gmail.com>
2024-02-13 12:45:44 -0600

    Ternary Operator

--- JavaScript/foundations/03_basicOperators.js
+++ JavaScript/foundations/03_basicOperators.js
@@ -159,5 +159,15 @@ console.log(hasDriversLicense || hasGoodVision); //true
 console.log(!hasGoodVision); //true
 console.log(!hasDriversLicense); //false
 
+//====CONDITIONAL (TERNARY) OPERATOR====//
+//Conditional (Ternary) Operator: the only JavaScript operator that takes three operands: a condition followed by a question mark (?), then an expression to execute if the condition is truthy followed by a colon (:), and finally the expression to execute if the condition is falsy. This operator is frequently used as an alternative to an if...else statement.
+function getFee(isMember) {
+  return isMember ? "$2.00" : "$10.00";
+}
+
+console.log(getFee(true)); // Expected output: "$2.00"
+console.log(getFee(false)); // Expected output: "$10.00"
+console.log(getFee(null)); // Expected output: "$10.00"
+
 //====OPERATOR PRECEDENCE====//
 //Operator precedence: determines how operators are parsed concerning each other. Operators with higher precedence become the operands of operators with lower precedence.
