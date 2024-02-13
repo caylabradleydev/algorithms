1bcb9572bf914102d02aa95525dddea248d348dc
Cayla Bradley <caylabradleydev@gmail.com>
2024-02-13 11:16:25 -0600

    Truthy and Falsy

---
+++ JavaScript/foundations/07_truthyAndFalsyValues.js
@@ -0,0 +1,12 @@
+//===============================//
+//====TRUTHY AND FALSY VALUES====//
+//===============================//
+
+//falsy: a value that is considered false when encountered in a Boolean context.  JavaScript determines falsy values implicitely when needed
+//there are 5 falsy values in JavaScript: 0, '', undefined, null, NaN
+
+console.log(Boolean(0)); //false
+console.log(Boolean("")); //false
+console.log(Boolean(null)); //false
+console.log(Boolean(undefined)); //false
+console.log(Boolean(NaN)); //false
