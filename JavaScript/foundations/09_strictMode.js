b7c71ee5282fa1ee1c6287259de6043c25526956
Cayla Bradley <caylabradleydev@gmail.com>
2024-02-14 08:43:30 -0600

    Strict Mode

---
+++ JavaScript/foundations/09_strictMode.js
@@ -0,0 +1,17 @@
+//===================//
+//====STRICT MODE====//
+//===================//
+
+// Strict mode makes several changes to normal JavaScript semantics:
+
+// Eliminates some JavaScript silent errors by changing them to throw errors.
+// Fixes mistakes that make it difficult for JavaScript engines to perform optimizations: strict mode code can sometimes run faster than identical code that’s not strict mode.
+// Prohibits some syntax likely to be defined in future versions of ECMAScript.
+//Strict mode makes it impossible to accidentally create global variables.
+//Strict mode makes assignments which would otherwise silently fail to throw an exception
+//Strict mode requires that function parameter names be unique.
+//Strict mode forbids setting properties on primitive values.
+
+"use strict"; //has to be the first line in code in order to work
+
+// The entire contents of JavaScript modules are automatically in strict mode, with no statement needed to initiate it.
