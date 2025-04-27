e5309db5676e5562fd792b61862699e6e558ad40
caylabradleydev <124472388+caylabradleydev@users.noreply.github.com>
2025-04-27 09:25:19 -0500

    Add solution - CodeHub

---
+++ 8-kyu/beginner-lost-without-a-map/beginner-lost-without-a-map.js
@@ -0,0 +1,20 @@
+function maps(x){
+  //NOTES
+  //input: array of integers
+  //output: NEW array with each value doubled
+  
+  //SOLUTION 1 (clean solution)
+  //map through given array, doubling each time
+  return x.map((y) => y * 2)
+  
+  //SOLUTION 2 (efficient solutoin for larger datasets)
+  //initialize new output array
+  //const output = []
+  
+  //iterate through given array
+//   for(let i = 0; i < x.length; i++){
+//     output.push(x[i] * 2)
+//   }
+  
+//   return output
+}
