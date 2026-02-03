fbbf3d9025464a4005f1bf2a503791607f879cef
caylabradleydev <124472388+caylabradleydev@users.noreply.github.com>
2026-02-02 21:10:43 -0600

    Add solution - CodeHub

---
+++ 8-kyu/contamination-number-1-string/contamination-number-1-string.js
@@ -0,0 +1,11 @@
+function contamination(text, char){
+  if(text === "" || char === "") return "";
+  
+  let str = "";
+  
+  for(i = 0; i < text.length; i++){
+    str += char[0]
+  }
+  
+  return str;
+}
