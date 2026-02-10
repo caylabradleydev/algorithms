be91a141f261b90c6d5d3fcc26ed52ba5bc11486
caylabradleydev <124472388+caylabradleydev@users.noreply.github.com>
2026-02-10 17:08:04 -0600

    Update solution - CodeHub

--- 8-kyu/l1-bartender-drinks/l1-bartender-drinks.js
+++ 8-kyu/l1-bartender-drinks/l1-bartender-drinks.js
@@ -1,13 +1,12 @@
 function getDrinkByProfession(param){
-  const drinkMenu = {
-  "jabroni":  "Patron Tequila",
-  "school counselor": "Anything with Alcohol",
-  "programmer": "Hipster Craft Beer",
-  "bike gang member": "Moonshine",
-  "politician": "Your tax dollars",
-  "rapper": "Cristal",
+  const menu = {
+    "jabroni": "Patron Tequila",
+    "school counselor": "Anything with Alcohol",
+    "programmer": "Hipster Craft Beer",
+    "bike gang member": "Moonshine",
+    "politician": "Your tax dollars",
+    "rapper": "Cristal"
   }
   
-  if(param.toLowerCase() in drinkMenu) return drinkMenu[param.toLowerCase()]
-  return "Beer"
+  return menu[param.toLowerCase()] || "Beer"
 }
